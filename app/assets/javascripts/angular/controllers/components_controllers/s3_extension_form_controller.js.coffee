af125d41c10180bcdc9b9f5f0378b97081e559a5
Jakub Bajorek <jakub-b@codetain.eu>
2016-03-21 11:26:42 +0100

    Change paths support for s3 extension

--- app/assets/javascripts/angular/controllers/components_controllers/s3_extension_form_controller.js.coffee
+++ app/assets/javascripts/angular/controllers/components_controllers/s3_extension_form_controller.js.coffee
@@ -1,12 +1,6 @@
 @csvv.controller 'S3ExtensionFormController', ['$scope', ($scope)->
 
-  set_form_layout = ()->
-    _.defer ()->
-      $( "#aws_region" ).select2({ theme: "bootstrap", width: '100%', allowClear: false, placeholder: "Select AWS region"}).
-        next( ".select2-container--bootstrap" ).addClass( "input-lg" );
-      
   angular.element(document).ready ()->
-    $scope.$ctrl.getFormDataPromise().then ()->
-      set_form_layout()
+    $scope.$ctrl.getFormDataPromise()
 
 ]
--- app/models/document_extensions/s3.rb
+++ app/models/document_extensions/s3.rb
@@ -10,9 +10,8 @@ module DocumentExtensions
       DocumentDecorators::S3
     end
     
-    def signed_link(uri)
-      parsed_uri = URI(uri)
-      "<a href='#{presigned_url(parsed_uri)}'>#{file_name(parsed_uri)}</a>"
+    def signed_link(filename)
+      "<a href='#{presigned_url(filename)}'>#{filename}</a>"
     end
     
     def aws_access_key_id
@@ -31,14 +30,24 @@ module DocumentExtensions
       extension_settings['aws_bucket']
     end
     
+    def aws_path
+      ap = extension_settings['aws_path']
+      return if ap.blank?
+      ap[0] == '/' ? ap[1..-1] : ap 
+    end
+    
     def columns
       extension_settings['columns'] || []
     end
     
     private
     
-    def presigned_url(uri)
-      s3_presigner.presigned_url(:get_object, bucket: aws_bucket, key: uri.path[1..-1])
+    def presigned_url(filename)
+      s3_presigner.presigned_url(:get_object, bucket: aws_bucket, key: aws_key(filename))
+    end
+    
+    def aws_key(filename)
+      Pathname.new(aws_path).join(filename).to_s
     end
     
     def file_name(uri)
