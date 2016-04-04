45f964ef2b5cc77a9b0ffb399739f7c20de64e69
andi <andreas.lahr@mobilityhouse.com>
2016-04-04 11:48:25 +0200

    reduced number of kept releases

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :deploy_to, '/home/deploy/apps/legacy-data.mobilityhouse.com'
 
 set :stage, :production
-set :keep_releases, 10
+set :keep_releases, 3
 role :app, %w{legacy-data.mobilityhouse.com}
 role :web, %w{legacy-data.mobilityhouse.com}
 role :db,  %w{legacy-data.mobilityhouse.com}
